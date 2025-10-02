0e98fe441ee9ea5cdfc224659af017a2df13ad89
victormori <victordaab@gmail.com>
2025-10-01 21:49:38 -0300

    atividades

---
+++ atividade.js
@@ -0,0 +1,9 @@
+// Atividade1
+
+let a = 10;
+let b = 20;
+
+console.log("soma dos valores=", a + b);
+console.log("subtração dos valores=", a - b);
+console.log("multiplicação dos valores=", a * b);
+console.log("divisão dos valores=", a / b);
